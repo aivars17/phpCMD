0fa32ef15e83015aee175a6906fb33cc6d78e6ba
aivars17 <30680298+aivars17@users.noreply.github.com>
2017-10-20 09:04:26 +0300

    update

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 /*
 echo "Hello world\n";
-echo bam(2,3);*/
+echo bam(2,3);
 function bam($a,$b) {
 	return $a + $b."\n";
 };
-/*
+
 for ($i=0; $i < 20; $i++) { 
 	echo "\nnumeravimas ".$i;
 	};
@@ -32,7 +32,7 @@ for ($i=5; $i > -5; $i-= 0.5) {
 	};
 	};
 
-echo $iter;*/
+echo $iter;
 
 function suma($a,$b){
 	$get = ($a * 1.5)*(1 + $b/100);
@@ -44,7 +44,7 @@ function plotas($a,$b,$c){
 	echo $get;
 }
 
-/*
+
 function bim($c,$b) {
 for ($i=0; $i < $c; $i++) { 
 	
@@ -116,7 +116,7 @@ function get_tiles($x,$z,$a,$b){
 	echo "Baseino dydis ". get_volume($x,$i,$z)."(".$i.")"." Pliteliu kiekis ".(ceil((get_volume($x,$i,$z))/($a*$b)))." vnt, reikes ".ceil((((get_volume($x,$i,$z))/($a*$b)))/9)."-ju pakuociu"."\n";
 	}
 }
-*/
+
 
 $users = [
 	["username" => "PovilasP","name" =>"Povilas","surname"=>"Povilaitis"],
@@ -140,6 +140,41 @@ $name = array_shift($argv);
 $funcName = array_shift($argv);
 
 call_user_func_array($funcName, $argv);
+*/
+function bam($a,$f,$d){
+	$start = microtime();
+for ($i=$a; $i >= 0; $i--) { 
+	 if ($i == $f or $i == $d) {
+		for ($c=$i; $c >= 0; $c--) { 
+			echo $c;
+		}
+		
+		echo "\n";
+
+	 } else {
+	 	echo $i."\n";
+	 }
+		
+	} 
+	echo "Duration ".(microtime().$start);
+	
+};
+
+/*
+function get_lowest(){
+	$temperatures = [28, 5, 3.5, 8, 6, 14, 20];
+	$lowest = $temperatures[0];
+	foreach ($temperatures as $t) {
+		if ($t < $lowest) {
+			$lowest = $t;
+		}
+		}
+		echo "lowest value ".$lowest;
+}*/
 
 
 
+$name = array_shift($argv);
+$funcName = array_shift($argv);
+
+call_user_func_array($funcName, $argv);
